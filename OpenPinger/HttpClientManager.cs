24761c0cddaf1c114fca8ecffd058562b778322f
Arthur <arthrp@users.noreply.github.com>
2020-04-02 22:37:09 +0300

    Adding httpclient manager

---
+++ OpenPinger/HttpClientManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace OpenPinger
+{
+    public class HttpClientManager
+    {
+        private IHttpClientFactory _clientFactory;
+
+        public HttpClientManager(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+    }
+}
